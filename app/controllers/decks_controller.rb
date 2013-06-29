4026b23ccd7bc5417c05073f8a5aca18f5d4e01a
Nicholas Wargnier <nwargnier@gmail.com>
2013-06-29 12:37:05 -0700

    javascript for card rendering after each guess

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -4,16 +4,17 @@ get '/decks' do
 end
 
 get '/deck/:id' do |id|
-  deck = Deck.find(id)
+  @deck = Deck.find(id)
   if !session[:round]
     session[:round] = 'yes'
-    @round = Round.create(user_id: session[:user_id], deck_id: deck.id)
+    @round = Round.create(user_id: session[:user_id], deck_id: @deck.id)
   end
-  @card = deck.cards.shuffle!.pop
   erb :"/decks/play"
 end
 
-get '/cards'
+
+post '/decks/:deck_id/:card_id' do |deck_id, card_id|
+  #DETERMINE IF GUESS IS CORRECT OR WRONG
+  content_type :json
+  {cardId: card_id, nextCard: (card_id.to_i + 1)}.to_json
 end
-
-
--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-  $.ajax({
-    url: 'get/cards',
-    method: 'get',
-    data:
-  })
-  .done(function(response){
-
+  $('.card1').first().show();
+  $('.guess').on("submit", function(e){
+    e.preventDefault();
+    var request = $(this).serialize();
+    $.ajax({
+      url: $(this).attr('action'),
+      method: 'post',
+      data: request
+    })
+    .done(function(response) {
+      // console.log($('.card' + response.cardId));
+      console.log(response.cardId);
+      $('.card' + response.cardId).remove();
+      $('.card' + response.nextCard).show();
+      console.log($(this));
+      console.log(response);
+    });
+    // console.log($(this).attr('action'));
   });
-
  });
